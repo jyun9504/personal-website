39f65741c9c028f461d3604b90fe88bdcf35d3a7
jyun9504 <jyun9504@gmail.com>
2020-07-14 10:08:15 +0800

    update works data

--- src/containers/Works/WorksCardsData.js
+++ src/containers/Works/WorksCardsData.js
@@ -1,25 +1,33 @@
 import ImgRobofilter from '../../assets/images/robofilter.png';
 import ImgJyunChat from '../../assets/images/jyunchat.png';
+import ImgJyunGeo from '../../assets/images/jyungeo.png';
 import ImgBuytea from '../../assets/images/buytea.png';
+
 
 const worksCards = [
   {
     img: ImgRobofilter, 
     title: 'Robo Filter', 
-    content: 'React App，使用 fetch 取得機器人 User 資料，機器人卡片可以對名字即時篩選。',
+    content: 'React App，fetch 取得機器人 User 資料，輸入框可用名字即時篩選卡片，使用 Redux 管理資料狀態。',
     src: 'https://jyun9504.github.io/robo-filter/'
   },
   {
     img: ImgBuytea, 
     title: 'Buy Tea', 
-    content: 'Vue App，localStorage 存取訂單資料。',
-    src: 'https://jyun-chat.web.app/'
+    content: 'Vue App，localStorage 儲存訂單資料，使用 Vuex 管理資料狀態。',
+    src: 'https://jyun9504.github.io/buytea/'
   },
   {
     img: ImgJyunChat, 
     title: 'Jyun Chat', 
-    content: 'Vue App，Firebase realtime database 即時存取聊天資料。',
+    content: 'Vue App，Firebase realtime database 即時更新聊天內容。',
     src: 'https://jyun-chat.web.app/'
+  },
+  {
+    img: ImgJyunGeo, 
+    title: 'Jyun Geo', 
+    content: 'Vue App，Firebase Authentication 驗證及管理使用者，realtime database 即時存取地理位置與留言板資訊。',
+    src: 'https://jyun-geo.web.app/'
   }
 ];
 
